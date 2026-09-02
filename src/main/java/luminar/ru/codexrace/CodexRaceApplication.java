aab16fbe302d51410ea463b2f8f8c284ec3727c5
grioo1main <grioo1main@gmail.com>
2026-09-02 21:01:17 +0300

    initial project structure

---
+++ src/main/java/luminar/ru/codexrace/CodexRaceApplication.java
@@ -0,0 +1,13 @@
+package luminar.ru.codexrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodexRaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodexRaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/luminar/ru/codexrace/CodexRaceApplicationTests.java
@@ -0,0 +1,13 @@
+package luminar.ru.codexrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodexRaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
